e47a7d42624ab7952c01cabc62dc42eaee605c63
1313ou <1313ou@gmail.com>
2024-05-25 11:17:12 +0200

    version set to 2.1.1

--- grind.sh
+++ grind.sh
@@ -23,5 +23,5 @@ fi
 mkdir -p "${OUTDIR}"
 echo "DIR:   ${OUTDIR}" 1>&2;
 
-jar=target/wndb2json-2.1.0-uber.jar
+jar=target/wndb2json-2.1.1-uber.jar
 java -ea -jar "${jar}" "${IN}" "${IN2}" "${OUTDIR}"
