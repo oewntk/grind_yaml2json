e38c931a8c4c1d3c99f86abc9055cfb32bc6bc64
1313ou <1313ou@gmail.com>
2026-05-25 10:04:16 +0200

    run from uber jar in cwd

--- grind.sh
+++ grind.sh
@@ -27,6 +27,17 @@ echo "OUT:   ${OUTFILE}" 1>&2;
 
 opts="-pretty"
 
-jar=target/yaml2json-2.4.0-uber.jar
+jar=yaml2json-2.4.0-uber.jar
+if [ ! -e "${jar}" ]; then
+  if [ ! -e "target/${jar}" ]; then
+    echo "Non existing uber jar" >&2
+    exit 1
+    fi
+  ln -s "target/${jar}"
+  fi
+if [ ! -e "${jar}" ]; then
+  echo "Non existing uber jar" >&2
+  exit 2
+  fi
 java -ea -jar "${jar}" ${opts} "${IN}" "${IN2}" "${OUTFILE}"
 
